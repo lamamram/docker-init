51f8409d1a594930b49d91dee66c71ab7b8f0def
mlamamra <mlamamra@dawan.fr>
2024-08-26 16:05:21 +0200

    stack_php

---
+++ 260824/stack_php/index.php
@@ -0,0 +1,20 @@
+<?php
+echo "<h1>Hello World from PHP8.2-FPM</h1>";
+echo 'Version PHP courante : ' . phpversion();
+
+// echo '<pre>';
+// try{
+  // $conn = new \PDO('mysql:host=????;dbname=????', '????', '?????');
+  // $sth = $conn->prepare('SELECT * FROM pays');
+  // $sth->execute();
+  // $checks = $sth->fetchAll(PDO::FETCH_ASSOC);
+  // foreach ($checks as $check) {
+
+  // print_r($check);
+  // }
+// }
+// catch(\Exception $e){
+    // print_r($e);
+// }
+// echo '</pre>';
+?>
