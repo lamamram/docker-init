21fd364ff95a2d100ce704ec5e704fbfac37c21f
mlamamra <mlamamra@dawan.fr>
2024-08-27 10:16:34 +0200

    mariadb with envs

--- 260824/stack_php/stack_php.sh
+++ 260824/stack_php/stack_php.sh
@@ -19,14 +19,27 @@ docker network create \
 
 
 ############################ CONTAINERS ###################################
+docker run \
+--name stack-php-db \
+-d --restart unless-stopped \
+--net stack_php \
+--env-file .env \
+mariadb:11.5
+
+# --env MARIADB_USER=test \
+# --env MARIADB_PASSWORD=roottoor \
+# --env MARIADB_DATABASE=test \
+# --env MARIADB_ROOT_PASSWORD=roottoor \
+
 
 docker run \
 --name stack-php-php8.2 \
 -d --restart unless-stopped \
 --net stack_php \
+-v ./index.php:/srv/index.php:ro \
 bitnami/php-fpm:8.2-debian-12
 
-docker cp index.php stack-php-php8.2:/srv/index.php
+# docker cp index.php stack-php-php8.2:/srv/index.php
 
 
 docker run \
@@ -34,9 +47,10 @@ docker run \
 -d --restart unless-stopped \
 -p 8080:80 \
 --net stack_php \
+-v ./vhost.conf:/etc/nginx/conf.d/vhost.conf:ro \
 nginx:1.27.1-alpine-slim
 # --link dns_php:stack_php_php8.2 (DEPECATED alias dns pour docker0)
 
-docker cp vhost.conf stack-php-web:/etc/nginx/conf.d/vhost.conf
-docker restart stack-php-web
+# docker cp vhost.conf stack-php-web:/etc/nginx/conf.d/vhost.conf
+# docker restart stack-php-web
 # en cas de -P port_number=$(docker ps -f | grep ...)
