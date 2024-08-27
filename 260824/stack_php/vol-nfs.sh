c0184b82b7a3ed5f14a17386302066b747597ab2
mlamamra <mlamamra@dawan.fr>
2024-08-27 12:10:27 +0200

    partage nfs

---
+++ 260824/stack_php/vol-nfs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# # ajout des éléments à utiliser à distance
+# sudo apt-get install -y nfs-kernel-server
+# sudo mkdir /mnt/nfs-dir
+# cp index.php php8.2.conf mariadb-init.sql /mnt/nfs-dir
+
+# # conf nfs "vanilla"
+# sudo chown -R nobody:nogroup /mnt/nfs-dir
+# sudo chmod 755 /mnt/nfs-dir
+# sudo chmod 644 /mnt/nfs-dir/*
+# # configuration server nfs
+# echo "/mnt/nfs-dir 192.168.1.0/24(rw,sync,no_subtree_check,no_all_squash)" | sudo tee -a /etc/exports
+# sudo exportfs -a
+# sudo systemctl restart nfs-kernel-server
+
+docker volume create \
+       --driver local \
+       --opt type=nfs \
+       --opt o=addr=192.168.1.30,ro \
+       --opt device=:/mnt/nfs-dir \
+       nfs-vol
+
+
