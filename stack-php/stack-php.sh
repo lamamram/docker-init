8b5e2920dbfb0897cae386b9dff022ef419f42eb
mlamamra <mlamamra@dawan.fr>
2024-12-16 17:34:41 +0100

    jour 1

--- stack-php/stack-php.sh
+++ stack-php/stack-php.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 #### SUPPRESSIONS #######
-
+# test de l'existence des conteneurs si c'est vrai je supprime les conteneurs
+# -q: affiche uniquement les identifiants
 [[ -z $(docker ps -aq --filter name="stack-php-*") ]] || docker rm -f $(docker ps -aq -f "name=stack-php-*")
 
-#### CONTENEUR
+#### CONTENEURS
 
 docker run \
        --name stack-php-nginx \
        -d --restart unless-stopped \
        -p 8080:80 \
        nginx:1.27.3-bookworm-perl
+
+docker run \
+       --name stack-php-fpm \
+       -d --restart unless-stopped \
+       bitnami/php-fpm:8.4-debian-12
+
