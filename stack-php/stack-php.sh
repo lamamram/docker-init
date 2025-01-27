234519163b5d3a25ad19b74d3f3915bc9a1011d6
dev_a <dev_a@gitlab.lan.fr>
2025-01-27 16:24:14 +0100

    stack-php: configs

--- stack-php/stack-php.sh
+++ stack-php/stack-php.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#### SUPPRESSIONS ####### 
+#### SUPPRESSIONS #######
 # test de l'existence des conteneurs si c'est vrai je supprime les conteneurs
 # -q: affiche uniquement les identifiants
 [[ -z $(docker ps -aq --filter name="stack-php-") ]] || docker rm -f $(docker ps -aq -f "name=stack-php-")
@@ -13,3 +13,9 @@ docker run \
        -d --restart unless-stopped \
        -p 8080:80 \
        nginx:1.27.3-bookworm
+
+docker run \
+       --name stack-php-fpm \
+       -d --restart unless-stopped \
+       bitnami/php-fpm:8.4-debian-12
+
