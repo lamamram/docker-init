47ba22922f762861274e0f5a6f238fa76ea5e357
lamamram <matthieu.lamamra@gmail.com>
2025-01-27 16:09:29 +0100

    MAJ stack-php.sh

--- stack-php/stack-php.sh
+++ stack-php/stack-php.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#### SUPPRESSIONS #######
+#### SUPPRESSIONS ####### 
 # test de l'existence des conteneurs si c'est vrai je supprime les conteneurs
 # -q: affiche uniquement les identifiants
 [[ -z $(docker ps -aq --filter name="stack-php-") ]] || docker rm -f $(docker ps -aq -f "name=stack-php-")
